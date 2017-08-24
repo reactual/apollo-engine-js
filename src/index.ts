d7d772faaa53473cedb824c3b3993fbb349f84f8
Aashish Welling <aashishwelling@gmail.com>
2017-08-24 15:26:22 -0700

    Move off portfinder (v0.3.1)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { randomBytes } from 'crypto'
+import { ChildProcess, execFile } from 'child_process';
+import { randomBytes } from 'crypto';
 import { readFileSync } from 'fs';
+import { createServer } from 'net';
 import { resolve } from 'path';
-import { ChildProcess, execFile } from 'child_process'
 
-import { getPortPromise } from 'portfinder'
-
-import { makeExpressMiddleware, makeConnectMiddleware, makeKoaMiddleware, instrumentHapi } from './middleware'
+import { makeExpressMiddleware, makeConnectMiddleware, makeKoaMiddleware, instrumentHapi } from './middleware';
 
 export type LogLevels = 'debug' | 'info' | 'warn' | 'error' | 'fatal';
 
@@ -102,39 +101,44 @@ export class Engine {
         const config = this.config;
         const endpoint = this.endpoint;
         const graphqlPort = this.graphqlPort;
-        return getPortPromise({
-            host: '127.0.0.1'
-        }).then((port) => {
-            this.enginePort = port;
-            const binaryPath = resolve(__dirname, '../bin', this.binary);
 
-            let child = this.child;
-            if (typeof config === 'string') {
-                const env = Object.assign({ 'ENGINE_CONFIG': port + ',' + endpoint + ',' + graphqlPort + ',' + this.headerSecret }, process.env);
-                child = execFile(binaryPath, ['-config=' + config, '-sload=true', '-restart=true'], { 'env': env }, (err: Error) => {
-                    if (err) console.error(err);
-                });
-                child.stdout.pipe(process.stdout);
-                child.stderr.pipe(process.stderr);
-                child.on('exit', () => {
-                    if (child != null) {
-                        throw new Error('Engine crashed unexpectedly');
-                    }
-                });
-            } else {
-                const sideloadConfig = JSON.parse(JSON.stringify(config));
-                sideloadConfig.frontends = [{ 'host': '127.0.0.1', 'endpoint': endpoint, 'port': port }];
-                sideloadConfig.origins = [{ url: 'http://127.0.0.1:' + graphqlPort + endpoint, headerSecret: this.headerSecret }];
-                const env = { 'env': Object.assign({ 'ENGINE_CONFIG': JSON.stringify(sideloadConfig) }, process.env) };
-                child = execFile(binaryPath, ['-config=env', '-restart=true'], env);
-                child.stdout.pipe(process.stdout);
-                child.stderr.pipe(process.stderr);
-                child.on('exit', () => {
-                    if (child != null) {
-                        throw new Error('Engine crashed unexpectedly')
-                    }
-                });
-            }
+        const srv = createServer();
+        return new Promise(resultPort => {
+            srv.on('listening', () => {
+                const port = srv.address().port;
+                srv.close();
+                this.enginePort = port;
+                const binaryPath = resolve(__dirname, '../bin', this.binary);
+
+                let child = this.child;
+                if (typeof config === 'string') {
+                    const env = Object.assign({ 'ENGINE_CONFIG': port + ',' + endpoint + ',' + graphqlPort + ',' + this.headerSecret }, process.env);
+                    child = execFile(binaryPath, ['-config=' + config, '-sload=true', '-restart=true'], { 'env': env }, (err: Error) => {
+                        if (err) console.error(err);
+                    });
+                    child.stdout.pipe(process.stdout);
+                    child.stderr.pipe(process.stderr);
+                    child.on('exit', () => {
+                        if (child != null) {
+                            throw new Error('Engine crashed unexpectedly');
+                        }
+                    });
+                } else {
+                    const sideloadConfig = JSON.parse(JSON.stringify(config));
+                    sideloadConfig.frontends = [{ 'host': '127.0.0.1', 'endpoint': endpoint, 'port': port }];
+                    sideloadConfig.origins = [{ url: 'http://127.0.0.1:' + graphqlPort + endpoint, headerSecret: this.headerSecret }];
+                    const env = { 'env': Object.assign({ 'ENGINE_CONFIG': JSON.stringify(sideloadConfig) }, process.env) };
+                    child = execFile(binaryPath, ['-config=env', '-restart=true'], env);
+                    child.stdout.pipe(process.stdout);
+                    child.stderr.pipe(process.stderr);
+                    child.on('exit', () => {
+                        if (child != null) {
+                            throw new Error('Engine crashed unexpectedly')
+                        }
+                    });
+                }
+                resultPort(port);
+            }).listen(0);
         });
     }
 
@@ -150,9 +154,9 @@ export class Engine {
         return makeKoaMiddleware(this.endpoint, 'http://127.0.0.1:' + this.enginePort, this.headerSecret);
     }
 
-   public instrumentHapiServer(server: any) {
-       instrumentHapi(server, this.endpoint, 'http://127.0.0.1:' + this.enginePort, this.headerSecret);
-   }
+    public instrumentHapiServer(server: any) {
+        instrumentHapi(server, this.endpoint, 'http://127.0.0.1:' + this.enginePort, this.headerSecret);
+    }
 
     public stop() {
         if (this.child == null) {
