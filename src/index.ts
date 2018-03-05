775996f783002d438f8b1b3c9c0e87b6273cfb58
David Glasser <glasser@davidglasser.net>
2018-03-05 14:40:36 -0800

    link to migration guide if you use the old API

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { ApolloEngineLauncher } from './launcher';
 export { ApolloEngine } from './engine';
+
+export class Engine {
+  public constructor() {
+    throw new Error(`As of apollo-engine 1.x, the Engine class has been replaced with a simpler API. See https://www.apollographql.com/docs/engine/1.0-migration.html to learn how to migrate.`);
+  }
+}
